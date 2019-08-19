877afa8e1cb43e3a2b41b2a013efc5c8c50bb5df
SHINPYO HONG <44682975+Andyhong99@users.noreply.github.com>
2019-08-19 10:36:05 -0400

    Add files via upload
    commit

---
+++ TextFieldApp/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  TextFieldApp
+//
+//  Created by Doug Jasper on 8/16/19.
+//  Copyright © 2019 triOS College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var myTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        myTextField.placeholder = "Email address here"
+        myTextField.textColor = UIColor.red
+        myTextField.font = UIFont(name: "Courier", size: 16)
+        myTextField.clearButtonMode = .whileEditing
+        }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0
+            }
+        }
+    }
+    
+    @IBAction func displayPassword(_ sender: UIButton) {
+        myTextField.text = passwordTextField.text?.uppercased()
+    }
+}
+
